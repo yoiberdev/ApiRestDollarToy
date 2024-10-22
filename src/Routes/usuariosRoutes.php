8d3cb6a21ee5673a3f0512b35fb51c23d67733c2
yoiber <yoiber696@gmail.com>
2024-10-21 22:57:04 -0500

    ....

--- src/Routes/usuariosRoutes.php
+++ src/Routes/usuariosRoutes.php
@@ -15,16 +15,16 @@ try {
 
     $controller = UsuarioController::createInstance();
 
-    $logFilePath = 'logs/' . date('Y-m-d') . '-usuario-log.txt';
-    $logHandler = new FileLogHandler($logFilePath);
-    $logger = new Log($logHandler);
+    // $logFilePath = 'logs/' . date('Y-m-d') . '-usuario-log.txt';
+    // $logHandler = new FileLogHandler($logFilePath);
+    // $logger = new Log($logHandler);
     
     $method = $_SERVER['REQUEST_METHOD'];
     $data = [];
 
     $id = isset($url[1]) ? (int)$url[1] : null;
 
-    $logger->writeLine('INFO', 'UsuarioController::handleRequest ' . $method);
+    // $logger->writeLine('INFO', 'UsuarioController::handleRequest ' . $method);
 
     switch ($method) {
         case 'GET':
@@ -37,7 +37,7 @@ try {
                 'id_rol' => isset($_GET['id_rol']) ? (int)$_GET['id_rol'] : null
             ];
 
-            $logger->writeLine('INFO', 'UsuarioController::handleRequest find' . json_encode($filters));
+            // $logger->writeLine('INFO', 'UsuarioController::handleRequest find' . json_encode($filters));
             $usuarios = $controller->handleRequest('find', $filters);
             echo $usuarios;
             break;
@@ -47,7 +47,7 @@ try {
             if ($url[0] === 'login') {
                 $result = $controller->login($body);
                 
-                $logger->writeLine('INFO', 'UsuarioController::handleRequest login ' . json_encode($body));
+                // $logger->writeLine('INFO', 'UsuarioController::handleRequest login ' . json_encode($body));
                 
                 echo $result;
                 break;
@@ -56,7 +56,7 @@ try {
                 
                 $body['password'] = password_hash($body['password'], PASSWORD_DEFAULT);
 
-                $logger->writeLine('INFO', 'UsuarioController::handleRequest create ' . json_encode($body));
+                // $logger->writeLine('INFO', 'UsuarioController::handleRequest create ' . json_encode($body));
             
                 echo $controller->handleRequest('create', $body);
             }
@@ -71,7 +71,7 @@ try {
             if (isset($body['password'])) {
                 $body['password'] = password_hash($body['password'], PASSWORD_DEFAULT);
             }
-            $logger->writeLine('INFO', 'UsuarioController::handleRequest update ' . json_encode($body));
+            // $logger->writeLine('INFO', 'UsuarioController::handleRequest update ' . json_encode($body));
             
             echo $controller->handleRequest('update', $body);
             break;
@@ -79,35 +79,35 @@ try {
         case 'DELETE':
             $jwtMiddleware->authenticate();
             $data['id'] = $id ?? null;
-            $logger->writeLine('INFO', 'UsuarioController::handleRequest delete ID ' . $data['id']);
+            // $logger->writeLine('INFO', 'UsuarioController::handleRequest delete ID ' . $data['id']);
 
             echo $controller->handleRequest('delete', $data);
             break;
 
         default:
-            $logger->writeLine('ERROR', 'Método no soportado: ' . $method);
+            // $logger->writeLine('ERROR', 'Método no soportado: ' . $method);
             http_response_code(405);
             echo json_encode(['error' => 'Método no soportado']);
             break;
     }
 } catch (ValidationException $e) {
-    $logger->writeLine('ERROR', 'ValidationException: ' . $e->getMessage());
+    // $logger->writeLine('ERROR', 'ValidationException: ' . $e->getMessage());
     http_response_code(400);
     echo json_encode(['error' => $e->getMessage()]);
 } catch (DataException $e) {
-    $logger->writeLine('ERROR', 'DataException: ' . $e->getMessage());
+    // $logger->writeLine('ERROR', 'DataException: ' . $e->getMessage());
     http_response_code(404);
     echo json_encode(['error' => $e->getMessage()]);
 } catch (\PDOException $e) {
-    $logger->writeLine('ERROR', 'PDOException: ' . $e->getMessage());
+    // $logger->writeLine('ERROR', 'PDOException: ' . $e->getMessage());
     http_response_code(500);
     echo json_encode(['error' => $e->getMessage()]);
 } catch (\Exception $e) {
-    $logger->writeLine('ERROR', 'Exception: ' . $e->getMessage());
+    // $logger->writeLine('ERROR', 'Exception: ' . $e->getMessage());
     http_response_code(500);
     echo json_encode(['error' => $e->getMessage()]);
 } catch (\TypeError $e) {
-    $logger->writeLine('ERROR', 'TypeError: ' . $e->getMessage());
+    // $logger->writeLine('ERROR', 'TypeError: ' . $e->getMessage());
     http_response_code(400);
     echo json_encode(['error' => $e->getMessage()]);
 }
